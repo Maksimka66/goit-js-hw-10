863e1a3529e36fd909426a90df8dafd05d93aaac
Maksimka66 <maxfese@gmail.com>
2024-01-07 15:57:15 +0200

    first task

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -3,8 +3,9 @@ import flatpickr from 'flatpickr';
 // Додатковий імпорт стилів
 import 'flatpickr/dist/flatpickr.min.css';
 
+const startButton = document.querySelector('[data-start]');
 let inputValue = document.querySelector('#datetime-picker');
-let userSelectedDate = 0;
+let userSelectedDate;
 
 flatpickr('#datetime-picker', {
   enableTime: true,
@@ -14,8 +15,31 @@ flatpickr('#datetime-picker', {
   onClose(selectedDates) {
     console.log(selectedDates[0]);
     userSelectedDate = inputValue.value;
-    if (userSelectedDate) {
-    } else {
+    if (userSelectedDate > this.defaultDate) {
+      startButton.removeAttribute('disabled');
+    } else if (userSelectedDate < this.defaultDate) {
+      window.alert('Please choose a date in the future');
     }
   },
 });
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+startButton.addEventListener('click', event => {});
