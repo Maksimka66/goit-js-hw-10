dca64e2af862a7838b5d77bbcf4f7f641586ad15
Maksimka66 <maxfese@gmail.com>
2024-01-07 04:22:51 +0200

    first task (begin)

---
+++ src/js/1-timer.js
@@ -0,0 +1,21 @@
+// Описаний в документації
+import flatpickr from 'flatpickr';
+// Додатковий імпорт стилів
+import 'flatpickr/dist/flatpickr.min.css';
+
+let inputValue = document.querySelector('#datetime-picker');
+let userSelectedDate = 0;
+
+flatpickr('#datetime-picker', {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+    userSelectedDate = inputValue.value;
+    if (userSelectedDate) {
+    } else {
+    }
+  },
+});
