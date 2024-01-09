7a3618a017626c841cf0c8c3ff5b2c58e173275a
Maksimka66 <140455421+Maksimka66@users.noreply.github.com>
2024-01-09 04:32:43 +0000

    Deploying to gh-pages from @ Maksimka66/goit-js-hw-10@9f2be0a85db8285fd7e19b9f29666e3201cd76a1 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,2 +1,2 @@
-import"./assets/modulepreload-polyfill-ec808ebb.js";import"./assets/vendor-e6f7761b.js";const t=document.querySelector('button[type="submit"]');t.addEventListener("click",()=>{});
+import"./assets/modulepreload-polyfill-ec808ebb.js";import"./assets/vendor-651d7991.js";const t=document.querySelector('button[type="submit"]');t.addEventListener("click",()=>{});
 //# sourceMappingURL=commonHelpers2.js.map
