8b936dc4b72a3434361ee33335e7fdfb9638d096
Maksimka66 <maxfese@gmail.com>
2024-01-09 19:43:21 +0200

    second task

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -4,5 +4,33 @@ import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
 const submitButton = document.querySelector('button[type="submit"]');
+const delayInput = document.querySelector('input[name="delay"]');
+const fulfilledRadio = document.querySelector('input[value="fulfilled"]');
+const rejectedRadio = document.querySelector('input[value="rejected"]');
 
-submitButton.addEventListener('click', () => {});
+submitButton.addEventListener('click', () => {
+  const delay = delayInput.value;
+  promise = new Promise((fulfilled, rejected) => {
+    setTimeout(() => {
+      if (fulfilledRadio.checked === true) {
+        fulfilled(delay);
+      } else if (rejectedRadio.checked === true) {
+        rejected(delay);
+      }
+    }, delay);
+  });
+
+  promise
+    .then(delay => {
+      iziToast.success({
+        title: `✅ Fulfilled promise in ${delay}ms`,
+        position: 'topRight',
+      });
+    })
+    .catch(delay => {
+      iziToast.error({
+        title: `❌ Rejected promise in ${delay}ms`,
+        position: 'topRight',
+      });
+    });
+});
